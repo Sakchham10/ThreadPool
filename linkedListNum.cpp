dfe80e131b26fe6be00118796b172622e1775292
PC <ssangroula10@outlook.com>
2026-07-06 22:17:54 -0500

    Creating new linkedList for test

---
+++ linkedListNum.cpp
@@ -0,0 +1,54 @@
+﻿//
+// Created by PC on 7/6/2026.
+//
+
+#include "linkedListNum.hpp"
+
+linkedListNum::linkedListNum() : head(nullptr) {}
+void linkedListNum::put(int value) {
+    std::unique_lock dataAccessLock(lock);
+    numNode *newNode = new numNode{value, nullptr};
+    if (head == nullptr) {
+        head = newNode;
+        dataAccessLock.unlock();
+        return;
+    }
+    auto curr = head;
+    while (curr->next != nullptr) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+    dataAccessLock.unlock();
+}
+
+void linkedListNum::findAndpop(int value) {
+    std::unique_lock dataAccessLock(lock);
+    auto curr = head;
+    numNode *prev = nullptr;
+    while (curr && curr->value != value) {
+        prev = curr;
+        curr = curr->next;
+    }
+    if (curr == nullptr) {
+        dataAccessLock.unlock();
+        return;
+    }
+    if (prev == nullptr) {
+        head = curr->next;
+    } else {
+        prev->next = curr->next;
+    }
+    delete curr;
+    dataAccessLock.unlock();
+}
+
+
+linkedListNum::~linkedListNum() {
+    std::unique_lock dataAccessLock(lock);
+    while (head != nullptr) {
+        numNode *temp = head;
+        head = head->next;
+        delete temp;
+    }
+    dataAccessLock.unlock();
+}
---
+++ linkedListNum.hpp
@@ -0,0 +1,26 @@
+﻿//
+// Created by PC on 7/6/2026.
+//
+
+#ifndef THREADSAFEDATASTRUCTURE_LINKEDLISTNUM_HPP
+#define THREADSAFEDATASTRUCTURE_LINKEDLISTNUM_HPP
+#include <mutex>
+
+
+struct numNode {
+    int value;
+    numNode *next;
+};
+class linkedListNum {
+    std::mutex lock;
+    numNode *head;
+
+public:
+    void put(int value);
+    void findAndpop(int value);
+    linkedListNum();
+    ~linkedListNum();
+};
+
+
+#endif // THREADSAFEDATASTRUCTURE_LINKEDLISTNUM_HPP
